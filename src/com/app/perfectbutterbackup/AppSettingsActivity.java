53b46ccdbbfb79c3f867017f8584a9a311a96ed9
Jonathan Marcus <advocat3@gmail.com>
2013-02-05 15:35:52 -0800

    added files for the each activity
    each activity for each tab, and the settings button, now have their own
    Java files and layout files.

---
+++ src/com/app/perfectbutterbackup/AppSettingsActivity.java
@@ -0,0 +1,9 @@
+package com.app.perfectbutterbackup;
+
+import android.app.Activity;
+
+// for any functionality needed when the user clicks the three dots at the top of the app and clicks the 'settings' entry
+public class AppSettingsActivity extends Activity
+{
+	
+}
---
+++ src/com/app/perfectbutterbackup/RestoreTabActivity.java
@@ -0,0 +1,25 @@
+package com.app.perfectbutterbackup;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
+import android.support.v4.app.FragmentActivity;
+
+//for any functionality that's needed for the restore tab
+public class RestoreTabActivity extends FragmentActivity implements ActionBar.TabListener 
+{
+
+	public void onTabReselected(Tab tab, FragmentTransaction ft) 
+	{
+		
+	}
+
+	public void onTabSelected(Tab tab, FragmentTransaction ft) 
+	{
+		
+	}
+
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) 
+	{
+		
+	}
+}
