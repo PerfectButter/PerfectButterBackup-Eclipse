50c493122c405fb6572ef91fe8812e51b2763701
Christopher Tugangui <ctugangui@gmail.com>
2013-02-07 17:11:05 -0800

    Corrected onCreateView to call in backuptabfragmentlayout

--- src/com/app/perfectbutterbackup/BackupTabFragment.java
+++ src/com/app/perfectbutterbackup/BackupTabFragment.java
@@ -1,4 +1,5 @@
 package com.app.perfectbutterbackup;
+
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -6,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 
 
 // for any functionality that's needed for the backup tab
@@ -19,9 +21,6 @@ public class BackupTabFragment extends Fragment
 	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
 	{
-		TextView textView = new TextView(getActivity());
-		textView.setGravity(Gravity.CENTER);
-		textView.setText("backup manager");
-		return textView;
+        return inflater.inflate(R.layout.backuptabfragmentlayout, container, false);
 	}
 }
